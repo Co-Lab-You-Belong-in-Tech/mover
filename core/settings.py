b3f341e8ef7877739090e70e6ed1133275e90e9e
sparkle666 <sikky606@gmail.com>
2023-08-31 15:50:21 +0100

    updated models

--- core/settings.py
+++ core/settings.py
@@ -135,10 +135,17 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# Media files
+
+MEDIA_ROOT = [BASE_DIR, 'media']
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'mover.CustomUser'
 
 # Django allauth
 
--- mover/models.py
+++ mover/models.py
@@ -1,37 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-# class Vehicle(models.Model):
-#     name = models.CharField(max_length=30)
-#     model = models.IntegerField()
-#     carphoto = models.ImageField()
-#     isAvailable = models.BooleanField(default=True)
-#     isLoading = models.BooleanField(default=True)
-#     vehicle_insurance = models.FileField()
+charging_rate = (
+    ("FX", "Fixed"),
+    ("HR", "Hourly"),
+)
 
-# class DriverSignUp(models.Model):
-#     id = models.IntegerField(primary_key=True)
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=30)
-#     isVerified = models.BooleanField(default=False)
-#     password = models.CharField(max_length=30)
-#     rateOfService = models.TextField()
-#     hourlyAmount =  models.IntegerField()
-#     fixedAmount = models.IntegerField()
-#     rating = models.IntegerField()
-#     location = models.TextField()
-#     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
-#     drivers_license = models.FileField()
-    
-# class CustomerSignup(models.Model):
-#     pass
+class CustomUser(AbstractUser):
+
+    address = models.TextField(max_length=300)
+
+    def __str__(self):
+        return self.first_name
 
 
-    
-# class Booking(models.Model):
-#     driver = models.ForeignKey(DriverSignUp, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=30)
-#     pickup_location = models.TextField()
-#     goods_names = models.CharField(max_length=225)
-#     goods_weight = models.IntegerField()
+class Driver(CustomUser):
+
+    is_verified = models.BooleanField(default=False)
+    rate_of_service = models.TextField(max_length=100, choices=charging_rate)
+    hourly_amount =  models.IntegerField()
+    fixed_amount = models.IntegerField()
+    rating = models.IntegerField()
+    drivers_license = models.FileField()
+    # user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name
+
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=200)
+    is_available = models.BooleanField(default=True)
+    is_loading = models.BooleanField(default=True)
+    vehicle_insurance = models.FileField()
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name
+
+class VehiclePhoto(models.Model):
+    photo = models.ImageField(upload_to="images")
+    vehicle_photo = models.ForeignKey(Vehicle, on_delete = models.CASCADE)
+
+
+class Booking(models.Model):
+    # name = models.CharField(max_length=300)
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE, related_name = "driver_bookings")
+    customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=True, related_name = "customer_bookings")
+    pickup_location = models.TextField(max_length=300)
+    dropoff_location = models.TextField(max_length=300)
+    goods_name = models.CharField(max_length=225)
+    total_weight = models.IntegerField()
+    total_fee = models.IntegerField()
+    is_fufuilled = models.BooleanField(default=False)
+
+
+class Goods(models.Model):
+    # Best to use a form set for this.
+    name = models.CharField(max_length=200)
+    weight = models.IntegerField(help_text="Weight in KG")
+    owner = models.ForeignKey(CustomUser, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
