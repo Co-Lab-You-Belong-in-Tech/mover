ac6e8824f22aa5992a803c62012a4d6dbb6514da
sparkle666 <sikky606@gmail.com>
2023-09-07 15:54:01 +0100

    working map

--- mover/urls.py
+++ mover/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import component, index, accept_request
 
 urlpatterns = [
-    path('', index, name = "index")
+    path('', index, name = "index"),
+    path('component/', component, name = "component"),
+    path('accept-request/', accept_request, name = "accept_request")
 ]
--- mover/views.py
+++ mover/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, "mover/map.html", {})
+    return render(request, "mover/index.html", {})
+
+def component(request):
+    return render(request, "mover/component.html", {})
+
+def accept_request(request):
+    return render(request, "mover/accept_request.html", {})
