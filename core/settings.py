11502ec84f47b6d3181f662bbd3884e98f34b5d6
sparkle666 <sikky606@gmail.com>
2023-09-07 12:18:09 +0100

    updated settings

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ptbs@=cuehdu4lkk$erga8j&)2d)+4s+pbfo9ma3e8uglrhpa)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["sparkle6.pythonanywhere.com"]
 
 
 # Application definition
--- mover/views.py
+++ mover/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, "mover/component.html", {})
+    return render(request, "mover/map.html", {})
