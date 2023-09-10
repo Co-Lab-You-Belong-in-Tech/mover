99ee9f2eece4eedb59981bc5515dde66bf47f646
sparkle666 <sikky606@gmail.com>
2023-09-10 01:54:38 +0100

    added more screens to template

--- mover/urls.py
+++ mover/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import component, driver_onboarding, index, accept_request, mover_details, request_mover
+from .views import component, driver_onboarding, index, accept_request, mover_details, ready_to_move, request_mover
 
 urlpatterns = [
     path('', index, name = "index"),
     path('component/', component, name = "component"),
     path('accept-request/', accept_request, name = "accept_request"),
     path('request-mover/', request_mover, name = "request_mover"),
+    path('ready-to-move/', ready_to_move, name = "ready_to_move"),
     path('mover-details/', mover_details, name = "mover_details"),
     path('driver-onboarding/', driver_onboarding, name = "driver_onboarding"),
 ]
--- mover/views.py
+++ mover/views.py
@@ -9,7 +9,10 @@ def component(request):
     return render(request, "mover/component.html", {})
 
 def accept_request(request):
-    return render(request, "mover/accept_request.html", {})
+    return render(request, "mover/driver_pages/accept_request.html", {})
+
+def ready_to_move(request):
+    return render(request, "mover/driver_pages/ready_to_move.html", {})
 
 def request_mover(request):
     return render(request, "mover/request_mover.html", {})
