a6f04edbf63dacbf1fc6694e6d6bbc3f24cb89d2
sparkle666 <sikky606@gmail.com>
2023-10-12 14:05:02 +0100

    removed reload from url

--- core/urls.py
+++ core/urls.py
@@ -27,9 +27,9 @@ urlpatterns = [
 
 if settings.DEBUG:
     # Add URLs for debugging purposes
-    urlpatterns += [
-        path("__reload__/", include("django_browser_reload.urls")),
+    # urlpatterns += [
+    #     path("__reload__/", include("django_browser_reload.urls")),
 
-    ]
+    # ]
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
